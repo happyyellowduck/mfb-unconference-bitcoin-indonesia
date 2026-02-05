e22cbd351c676407a38dfd33567c25146f22586f
happyyellowduck <mariusmosalah@gmail.com>
2026-02-05 17:12:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-// Output static HTML safely
+// Simple theme loader for WordPress (serves index.html)
 $path = get_stylesheet_directory() . '/index.html';
-
 if (file_exists($path)) {
     echo file_get_contents($path);
 } else {
